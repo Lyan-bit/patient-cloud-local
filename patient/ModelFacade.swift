051e8c1488b44b48a1cb56018c52e25b9e9a13c8
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 22:40:42 +0100

    Update ModelFacade.swift

--- patient/ModelFacade.swift
+++ patient/ModelFacade.swift
@@ -2,63 +2,6 @@
 import Foundation
 import SwiftUI
 
-/* This code requires OclFile.swift */
-
-func initialiseOclFile()
-{ 
-  //let systemIn = createByPKOclFile(key: "System.in")
-  //let systemOut = createByPKOclFile(key: "System.out")
-  //let systemErr = createByPKOclFile(key: "System.err")
-}
-
-/* This metatype code requires OclType.swift */
-
-func initialiseOclType()
-{ let intOclType = createByPKOclType(key: "int")
-  intOclType.actualMetatype = Int.self
-  let doubleOclType = createByPKOclType(key: "double")
-  doubleOclType.actualMetatype = Double.self
-  let longOclType = createByPKOclType(key: "long")
-  longOclType.actualMetatype = Int64.self
-  let stringOclType = createByPKOclType(key: "String")
-  stringOclType.actualMetatype = String.self
-  let sequenceOclType = createByPKOclType(key: "Sequence")
-  sequenceOclType.actualMetatype = type(of: [])
-  let anyset : Set<AnyHashable> = Set<AnyHashable>()
-  let setOclType = createByPKOclType(key: "Set")
-  setOclType.actualMetatype = type(of: anyset)
-  let mapOclType = createByPKOclType(key: "Map")
-  mapOclType.actualMetatype = type(of: [:])
-  let voidOclType = createByPKOclType(key: "void")
-  voidOclType.actualMetatype = Void.self
-	
-  let patientOclType = createByPKOclType(key: "Patient")
-  patientOclType.actualMetatype = Patient.self
-
-  let patientPatientId = createOclAttribute()
-  	  patientPatientId.name = "patientId"
-  	  patientPatientId.type = stringOclType
-  	  patientOclType.attributes.append(patientPatientId)
-  let patientName = createOclAttribute()
-  	  patientName.name = "name"
-  	  patientName.type = stringOclType
-  	  patientOclType.attributes.append(patientName)
-  let patientAppointmentId = createOclAttribute()
-  	  patientAppointmentId.name = "appointmentId"
-  	  patientAppointmentId.type = stringOclType
-  	  patientOclType.attributes.append(patientAppointmentId)
-  let appointmentOclType = createByPKOclType(key: "Appointment")
-  appointmentOclType.actualMetatype = Appointment.self
-
-  let appointmentAppointmentId = createOclAttribute()
-  	  appointmentAppointmentId.name = "appointmentId"
-  	  appointmentAppointmentId.type = stringOclType
-  	  appointmentOclType.attributes.append(appointmentAppointmentId)
-  let appointmentCode = createOclAttribute()
-  	  appointmentCode.name = "code"
-  	  appointmentCode.type = stringOclType
-  	  appointmentOclType.attributes.append(appointmentCode)
-}
 
 func instanceFromJSON(typeName: String, json: String) -> AnyObject?
 	{ let jdata = json.data(using: .utf8)!
@@ -91,8 +34,7 @@ class ModelFacade : ObservableObject {
 	static func getInstance() -> ModelFacade { 
 		if instance == nil
 	     { instance = ModelFacade() 
-	       initialiseOclFile()
-	       initialiseOclType() }
+         }
 	    return instance! }
 	                          
 	init() { 
